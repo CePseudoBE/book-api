9fe7cb80723259fdeeeab97d495e3b89ca830f50
CePseudo <axel.hoffmann0205@yahoo.com>
2024-07-28 18:11:54 +0200

    index get books from genre and all

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -4,6 +4,8 @@ using BookApi.Helpers;
 using BookApi.Validator;
 using Microsoft.Net.Http.Headers;
 using System.Text.Json;
+using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookApi.Controllers
 {
@@ -21,6 +23,37 @@ namespace BookApi.Controllers
             _context = context;
             _loggingHelper = loggingHelper;
             _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("", Name = "CreateBooks")]
+        public async Task<IActionResult> Index([FromQuery] string[] genres)
+        {
+            List <Book> books;
+            try
+            {
+               if (genres != null && genres.Length > 0)
+                {
+                    books = await _context.Books
+                        .Include(b => b.Genres)
+                        .Include(b => b.Author)
+                        .Where(b => b.Genres.Any(g => genres.Contains(g.Name)))
+                        .ToListAsync();
+                }
+                else
+                {
+                    books = await _context.Books
+                        .Include(b => b.Genres)
+                        .Include(b => b.Author)
+                        .ToListAsync();
+                }
+                
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                _loggingHelper.LogError(ex, "An error occurred while getting the books.");
+                return StatusCode(500, "Internal server error");
+            }
         }
 
         [HttpPost("", Name = "CreateBooks")]
